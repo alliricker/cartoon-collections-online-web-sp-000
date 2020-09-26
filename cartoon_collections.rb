829fd2e20f03aeea80d644b62a766db1a9b110b2
Allison Ricker <alliricker@gmail.com>
2020-09-26 02:53:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 
 def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheeses.select! { |cheese| cheese_types.include? (cheese) }
+  cheeses.find { |cheese| cheese_types.include? (cheese) }
 end
